29edc74f8f586d14bcc3dcfe55eeaedbdf80a100
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-08-08 13:06:32 +0300

    Fixed typo

--- main.h
+++ main.h
@@ -1,4 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_number(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int _putchar(char c);
+
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
+
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p;
 #endif
