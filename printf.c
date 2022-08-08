f51668a334772b8c69aa0d076f9d5eb1f558d57e
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-08-08 12:58:17 +0300

    Initial commit

---
+++ printf.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * conv_char -function for conversion char
+ *@c: char to check
+ * Return: pointer to function
+ */
+
+int (*conv_char(const char c))(va_list)
+{
+	int i = 0;
+
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_number},
+    {"i", print_number},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int sum = 0, i = 0;
+	int (*func)();
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+				func = conv_char(format[i + 1]);
+			if (func == NULL)
+			{
+				_putchar(format[i]);
+				sum++;
+				i++;
+			}
+			else
+			{
+				sum += func(ap);
+				i += 2;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+			i++;
+		}
+	}
+	va_end(ap);
+	return (sum);
+}
