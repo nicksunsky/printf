deb249873d57a81880c7925b3e7c857f25516eed
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-08-08 18:45:58 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,41 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_nbr(va_list ap);
-int print_binary(va_list ap);
-int print_octal(va_list ap);
-int print_hexa_lower(va_list ap);
-int print_hexa_upper(va_list ap);
-int print_unsigned(va_list ap);
-int print_str_unprintable(va_list ap);
-int print_str_reverse(va_list ap);
-int print_ptr(va_list ap);
-int print_rot13(va_list ap);
-int print_percent(va_list ap __attribute__((unused)));
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
+#include <limits.h>
+
 int _putchar(char c);
-int _puts(char *str, int ascii);
-int _nbr_len(int prmNumber);
-int _strlen_recursion(char *s);
-int convert_alpha_numeric(int nb, int upper);
-char *convert_rot13(char *str);
-char *convert_base(unsigned long nb, unsigned int base, int upper);
-char *_strdup(char *str);
-char *convert_base_pointer(unsigned long p);
+int _printf(const char *format, ...);
+int switch_num(va_list, char, int);
+int switch_char(va_list, char, int);
+int print_char(va_list, int *);
+int print_string(va_list, int *, char);
+int print_num(va_list, int *, char);
+char *num_converter(unsigned int, unsigned int *);
+char *hex_converter(unsigned int, unsigned int *, char);
+int count_digits(unsigned int, unsigned int *);
+int out_num(char *, int, int *);
+char *bnten(int, char, unsigned int *);
+void rev_string(char *, int);
+char *rot13(char *, int);
 
-/**
- * struct flags_printf - struct conversion to function
- * @c: flag string
- * @f: pointer to func
- */
-
-typedef struct flags_printf
-{
-	char *c;
-	int (*f)(va_list);
-} flags_p;
 #endif
