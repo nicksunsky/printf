611117a19c402686ae12ac3d38ab9d55caf2457b
nicksunsky <66879557+nicksunsky@users.noreply.github.com>
2022-08-08 13:24:08 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,22 +1,31 @@
-#include "main.h"
 #include <stdarg.h>
+#include "main.h"
 #include <stddef.h>
 
 /**
- * conv_char -function for conversion char
- *@c: char to check
+ * get_op - select function for conversion char
+ * @c: char to check
  * Return: pointer to function
  */
 
-int (*conv_char(const char c))(va_list)
+int (*get_op(const char c))(va_list)
 {
 	int i = 0;
 
 	flags_p fp[] = {
 		{"c", print_char},
-		{"s", print_string},
-		{"d", print_number},
-   	        {"i", print_number},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
 		{"%", print_percent}
 	};
 	while (i < 14)
@@ -51,7 +60,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			if (format[i + 1] != '\0')
-				func = conv_char(format[i + 1]);
+				func = get_op(format[i + 1]);
 			if (func == NULL)
 			{
 				_putchar(format[i]);
